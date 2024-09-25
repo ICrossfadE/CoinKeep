4da3016b7a6b67d65dac1b0e63832c5fd9acbb41
Maks <Crossfade143@gmail.com>
2024-09-25 16:08:27 +0300

    fix problem with getWallets

--- lib/presentation/screens/dashboard/DashboardScreen.dart
+++ lib/presentation/screens/dashboard/DashboardScreen.dart
@@ -1,4 +1,3 @@
-import 'package:CoinKeep/logic/blocs/getWallet_cubit/get_wallet_cubit.dart';
 import 'package:CoinKeep/logic/blocs/local_cache_bloc/local_cache_bloc.dart';
 import 'package:CoinKeep/presentation/screens/dashboard/BottomNavItems.dart';
 import 'package:CoinKeep/src/theme/dark.dart';
@@ -37,9 +36,6 @@ class _DashboardPageState extends State<DashboardScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // Колірна схема
-    // final colorScheme = Theme.of(context).colorScheme;
-
     return BlocProvider(
       create: (context) => _coinsBloc,
       child: Scaffold(
@@ -67,19 +63,7 @@ class _DashboardPageState extends State<DashboardScreen> {
             builder: (context) {
               return Center(
                 // Динамічний список віджетів
-                child: BlocBuilder<GetWalletCubit, GetWalletState>(
-                  builder: (context, state) {
-                    if (state.wallets.isEmpty) {
-                      return const Center(
-                          child: Text(
-                        'No Wallets found.',
-                        style: TextStyle(color: Colors.amber),
-                      ));
-                    }
-                    return BottomNavItems.getWidgets(state.wallets)
-                        .elementAt(_selectedIndex);
-                  },
-                ),
+                child: BottomNavItems.getWidgets().elementAt(_selectedIndex),
               );
             },
           ),
