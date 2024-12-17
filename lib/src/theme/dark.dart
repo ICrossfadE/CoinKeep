3af50641ba2305925f2ea1b2bf6e34f0ae6bd43f
Maks <Crossfade143@gmail.com>
2024-12-17 16:05:54 +0200

    upd colors

--- lib/src/theme/dark.dart
+++ lib/src/theme/dark.dart
@@ -1,7 +1,7 @@
 import 'dart:ui';
 
 // Colors
-const kDarkBg = Color(0xFF080b11);
+const kDarkBg = Color(0xFF171717);
 const kDark500 = Color(0xFF1A1E21);
 const kDark400 = Color(0xFF253745);
 // const kDark300 = Color(0xFF4A5C6A);
--- lib/src/theme/light.dart
+++ lib/src/theme/light.dart
@@ -1,10 +1,10 @@
 import 'dart:ui';
 
 // Colors
-const kLightBg = Color(0xFFCCD0CF);
-const kLight500 = Color(0xFFD6E8EE);
-const kLight400 = Color(0xFF97CADB);
-const kLight300 = Color(0xFF018ABE);
-const kLight200 = Color(0xFF02457A);
-const kLight100 = Color(0xFF06141B);
+const kLightBg = Color(0xFFDBEDF3);
+const kLight500 = Color(0xFF9AA6B2);
+// const kLight400 = Color(0xFF97CADB);
+const kLight300 = Color(0xFF00818A);
+const kLight200 = Color(0xFF404B69);
+const kLight100 = Color(0xFF283149);
 //============================
